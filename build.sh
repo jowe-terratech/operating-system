46c5c5cf2aa26886b11cffe3a1cd69ef5b11b02b
jowe <j.westenhoefer@bluelotus.ai>
2024-05-27 22:45:23 +0200

    Hello-World!

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create the out/ directory if it does not exist
+mkdir -p out
+
+# Assemble the boot sector code
+nasm -f bin boot.asm -o out/boot.bin
+
+# Create a blank USB image of 64MB
+dd if=/dev/zero of=out/usb.img bs=1M count=64 
+
+# Write the boot sector to the USB image
+dd if=out/boot.bin of=out/usb.img conv=notrunc
+
+# Run the USB image in QEMU
+qemu-system-x86_64 -m 1024 -drive format=raw,file=out/usb.img
