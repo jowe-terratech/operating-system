ca723f9e5ba096cc978e316cd4bbb9b3a16bbc89
jowe <j.westenhoefer@bluelotus.ai>
2024-06-16 18:04:01 +0200

    Removed non-working sprint methods

--- src/bootloader/stage-2.asm
+++ src/bootloader/stage-2.asm
@@ -3,40 +3,32 @@
 ; ======================================================
 BITS 16
 section .stage2  ; This defines the entry point of the second stage bootloader
+jmp main
 
-start:
-    mov ax, 0x8000      ; The address where we loaded the second stage bootloader
-    mov ds, ax          ; Set the data segment to the address where we loaded the second stage bootloader
-    mov es, ax          ; Set the extra segment to the address where we loaded the second stage bootloader
-    mov fs, ax          ; Set the fs segment to the address where we loaded the second stage bootloader
-    mov gs, ax          ; Set the gs segment to the address where we loaded the second stage bootloader
-    mov ss, ax          ; Set the stack segment to the address where we loaded the second stage bootloader
+setup:
+    ; Prepare the segments for the second stage bootloader
+    mov ax, 0x8000      
+    mov ds, ax          
+    mov es, ax          
+    mov fs, ax          
+    mov gs, ax          
+    mov ss, ax
+    ret 
 
+main:
+    call setup 
+    ; Empty screen and set cursor to the top left corner
     call clear_screen
+    xor dx, dx
+    call cursor_set_pos
+
+    mov al, 0x30
+    call cprint
 
     cli            ; Disable interrupts
     hlt            ; Halt the CPU
     jmp $          ; Infinite loop
 
-clear_screen:
-    ; Save variables that will be used
-    push di
-    pusha
-
-    ; Setup video-memory
-    mov ax, 0xb800  ; Video memory segment (color text mode)
-    mov es, ax      ; Set Extra Segment to video memory segment
-    mov di, 0       ; Start at the beginning of video memory
-    mov cx, 2000    ; Clear 80 columns * 25 rows * 2 bytes (character and attribute)
-
-clear_screen_main:
-    ; Print " " (space) character with attribute 0x0f (white on black)
-    mov ax, 0x0f20          ; Space character in attribute 0x0f (white on black)
-    stosw                   ; Store ax at es:di and increment di by 2
-    loop clear_screen_main  ; Loop cx times
-
-clear_screen_end:
-    ; Restore variables
-    popa
-    pop di
-    ret
+%include "src/bootloader/bios/cursor.asm"
+%include "src/bootloader/video-memory/clear-screen.asm"
+%include "src/bootloader/video-memory/sprint16.asm"
--- src/bootloader/video-memory/sprint16.asm
+++ src/bootloader/video-memory/sprint16.asm
@@ -1,75 +1,10 @@
-sprint:
-    ; ==========================================
-    ; sprint - Print a null-terminated string using the video memory
-    ; Arguments: 
-    ; - si points to the string
-    ; ==========================================
-    pusha
-    push es 
-    jmp .sprint_start
-
-    .sprint_next:
-        call cprint                 ; Print the next character
-
-    .sprint_start:
-        mov ax, 0xb800              ; Video memory segment (color text mode)
-        mov es, ax                  ; Set Extra Segment to video memory segment
-        cld                         ; Clear the direction flag
-        lodsb                       ; Load byte at ds:si into al, increment si because di is clear
-        cmp al, 0                   ; Check if byte is null (end of string)
-        jne .sprint_next             ; If null, end
-
-    pop es
-    popa
-    ret
-
-sprint_hex:
-    ; ==========================================
-    ; sprint_hex - Print a 16-bit number as a 4-digit hex string using the video memory
-    ; Arguments:
-    ; - al contains the value to print
-    ; ==========================================
-
-    ; Clear registers we will use, except al as that is the input
-    xor ah, ah
-    xor bx, bx
-    xor cx, cx
-
-    ; Build a 4-digit hex string
-    mov cx, 0x04                ; Set loop counter to 4
-
-    .hex_loop:
-        rol ax, 0x04                ; Rotate ax left by 4 bits (1 digit)
-        mov bl, al                  ; Move al to bl
-        and bl, 0x0f                ; Mask the upper 4 bits
-        
-        or bl, bl                  ; Check if we reached the end of the number
-        je .done                    ; If we did, we are done
-
-        cmp bl, 0x09                ; Check if its a number
-        jbe .number                 ; If it is, jump to .number
-        add bl, 0x07                ; If not, add 7 to get the correct ASCII value of a-f
-
-    .number:
-        add bl, 0x30                ; Convert to ASCII
-        mov al, bl                  ; Move bl to al
-        call cprint                 ; Print the character
-        loop .hex_loop              ; Loop until cx is 0
-
-    .done:
-
-    ret
-
 cprint:
     ; ==========================================
     ; cprint - Print a single character using the video memory
     ; Arguments:
     ; - al contains the character to print
     ; ==========================================
-    ; Safe registers: ax, bx, cx
-    push ax
-    push bx
-    push cx
+    pusha
 
     mov ah, 0x0f                    ; Set the attribute to white on black
     mov cx, ax                      ; Save character+attribute in cx
@@ -100,11 +35,7 @@ cprint:
         mov dh, [sprint_ypos]           ; Move sprint_ypos to dh
         call cursor_set_pos             ; Update the visible cursor accordingly
 
-    ; Restore registers
-    pop cx
-    pop bx
-    pop ax
-
+    popa 
     ret
 
 sprint_newline:
